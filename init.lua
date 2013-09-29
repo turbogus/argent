b8c5b510ededdf9d63e7aad31fa255e11683852d
Zaraki98 <superlagron@hotmail.fr>
2013-09-29 12:32:37 +0200

    Meilleure présentation du init.lua

--- init.lua
+++ init.lua
@@ -407,11 +407,11 @@ Convertions :
 - 10 pièces en charbon = 1 pièce en etain
 - 50 pièces en charbon = 5 pièces en etain = 1 pièce en cuivre
 - 100 pièces en charbon = 10 pièces en etain = 2 pièces en cuivre = 1 pièce en acier
-- 1.000 pièces en charbon = 100 pièces en etain = 20 pièces en cuivre = 10 pièces en acier = 1 billet de 10 // 1 billet de 10 = 1 feuille à billet + silver_ingot + tampon
-- 2.000 pièces en charbon = 200 pièces en etain = 40 pièces en cuivre = 20 pièces en acier = 1 billet de 20 // 1 billet de 20 = 1 feuille à billet + gold_ingot + tampon
-- 5.000 pièces en charbon = 500 pièces en etain = 100 pièces en cuivre = 50 pièces en acier = 1 billet de 50 // 1 billet de 50 = 1 feuille à billet + mese_crystal + tampon
-- 10.000 pièces en charbon = 1.000 pièces en etain = 200 pièces en cuivre = 100 pièces en acier = 1 billet de 100 // 1 billet de 100 = 1 feuille à billet + diamond_crystal + tampon
-- 20.000 pièces en charbon = 2.000 pièces en etain = 400 pièces en cuivre = 200 pièces en acier = 1 billet de 200 // 1 billet de 200 = 1 feuille à billet + 2 diamond_crystal + tampon
-- 50.000 pièce en charbon = 5.000 pièces en etain = 1.000 pièces en cuivre = 500 pièces en acier = 1 billet de 500 // 1 billet de 500 = 1 feuille à billet + mese_block + tampon
+- 1.000 pièces en charbon = 100 pièces en etain = 20 pièces en cuivre = 10 pièces en acier = 1 billet de 10 
+- 2.000 pièces en charbon = 200 pièces en etain = 40 pièces en cuivre = 20 pièces en acier = 1 billet de 20 
+- 5.000 pièces en charbon = 500 pièces en etain = 100 pièces en cuivre = 50 pièces en acier = 1 billet de 50 
+- 10.000 pièces en charbon = 1.000 pièces en etain = 200 pièces en cuivre = 100 pièces en acier = 1 billet de 100 
+- 20.000 pièces en charbon = 2.000 pièces en etain = 400 pièces en cuivre = 200 pièces en acier = 1 billet de 200 
+- 50.000 pièce en charbon = 5.000 pièces en etain = 1.000 pièces en cuivre = 500 pièces en acier = 1 billet de 500
 
 ]]--
