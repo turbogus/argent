94e925e41847486dc8ed00b98e6fc58f61521c4e
turbogus <g-baheu59@orange.fr>
2013-10-07 22:06:15 +0200

    Reprise intégrale du mod
    - Version 1.0 du mod.
    
    - Enlevé les centimes.
    - Ajouté pièce de 5.
    - Ajout du bloc cash (acceccible uniquement par admin via /giveme et incassable ).
      Il faut cliquer sur le bloc avec du cobble dans son menu pour l'échanger contre des pièce de 1€
    
    - Création d'une méthode de définition des prix des items via la valeur ajoutée des crafts et des matériaux nobles ( expliqué en commentaire ).
    
    - Création des conversion et conversion inverse ( on converti son argent directement dans la zone de craft ).
    
    Reste à faire :
    
    - Texture du blocs cash.
    - Création d'un menu style animals_npc pour le cube cash.

--- init.lua
+++ init.lua
@@ -1,447 +1,337 @@
 --[[
 
-Argent, un petit mod permettant de créer une économie sur un serveur minetest.
-Créé par turbogus, Zaraki98 et Ze_Escrobar
-Code et graphisme en GPL
+Mod ARGENT par turbogus, Zaraki98 et Ze_Escrobar
+code en licence GPL v2 ou supérieur
+graphisme en licence CC-BY-SA
 
+Version 1.0
+
+**********************************************************************************************
+
+Nom de la monnaie de minetest : ???
+
+base de conversion : 10
+
+base 10 : 1€ / 5€ / 10€ / 20€ / 50€ / 100€ / 200€ / 500€
+
+
+**********************************************************************************************
+
+Bloc de référence , le cobble :
+
+- 1 cobble = 3€
+
+- Une pioche en mese permet de digger 450 blocs de cobble. 
+
+Il faut 3 cristaux de mese pour faire une pioche :
+	--> valeur en cobble d'un cristal = 450 / 3 = 150
+
+1 bloc de mese = 9 cristaux de mese :
+	--> valeur en cobble d'un bloc de mese = 150*9 = 1350
+
+	
+- Une pioche en steel permet de digger 180 blocs de cobble.
+
+Il faut 3 blocs d'acier pour faire une pioche :
+	--> valeur en cobble d'un bloc d'acier = 180 / 3 = 60
+	
+	
+- Une pioche en cobble permet de digger 63 blocs de cobble.
+
+--> Nous donne un rapport de 3 / 63 = 0.04 ( 63 blocs de cobble récupérés pour 3 blocs de cobble dépensés(craft de la pioche) ). 
+   Il faut 8 actions pour crafter une pioche en cobble :
+
+1- Récupérer du tree
+2- Crafter du wood
+3- Crafter des stick
+4- crafter une pioche en bois
+5- digger 1 cobble
+6- digger 1 cobble
+7- digger 1 cobble
+8- crafter la pioche en pierre
+
+Donc (1 / 8) / 0.04 : 3.125 (valeur ajoutée)
+
+******************************************
+*On peut ramener le prix d'un cobble à 3€*
+******************************************
+
+Prix si un bloc de cobble rapporte 3€
+
+- Diamond = mese * 2 = 1350*3*2 = 8100€
+- mese : 1350 *3 = 4050€
+- steel : 60 * 3 = 180€
+
+Avec le rapport de 0.04 et le nombre d'actions à effectuer pour le crafter, on peu définir le prix d'un objet. On peut également utiliser le prix des matières
+nobles si elles rentres dans la fabrication de l'objet !
+
+***
+
+ex : épée en en mese :
+
+Composition 1 stick et 2 cristaux de mese
+
+Nombres d'action pour obtenir des sticks : 3 ( couper du tree, crafter du wood, crafter des sticks(4) ).
+--> valeur ajoutée (1 / 3) / 0.04 = 8.33333 ~ 9
+
+Prix d'un cristal de mese : 450€
+
+--> prix d'une épée en mese : 450 + 9 = 459€ ~ 460€
+
+***
+
+ex2 : hache en steel_ingot :
+
+Nbres d'actions pour 1 steel_ingot : 1 ( cuir 1 iron lump dans un four )
+-->Valeur ajoutée : 1/1/0.04 = 25
+
+Nbres d'actions pour des sticks(x4) : 3
+-->Valeur ajoutée 1/3/0.04 = ~9
+
+prix d'un steel ingot : 60€
+
+prix d'une hache en steel : 9 + 3*(60+25)= 264€
+
+**********************************************************************************************
 ]]--
 
---**************************************************************************************
+--++++++++++++
+--|piece de 1|
+--++++++++++++
 
---J'AI DIS, ON LAISSE COMMENTé !!!!!!
-
---[[
---Poinçon à pièces :
-minetest.register_craftitem("argent:poincon", {
-    description = "poincon",
-    inventory_image = "poincon.png",
+--Declaration
+minetest.register_craftitem("argent:piece1", {
+description = " piece de 1",
+inventory_image = "piece_acier.png",
 })
 
+--Conversion inverse
 minetest.register_craft({
-    output = "argent:poincon",
-    recipe = {
-        {"","default:stick",""},
-        {"","default:stick",""},
-        {"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
-    }
-})
-
---Tampon à billet :
-minetest.register_craftitem("argent:tampon", {
-    description = "tampon",
-    inventory_image = "tampon.png",
-})
-
-minetest.register_craft({
-    output = "argent:tampon",
-    recipe = {
-        {"","default:stick",""},
-        {"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
-        {"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
-    }
-})
-
-]]--
---**************************************************************************************
---Pièce en charbon (1 centimes) :
-minetest.register_craftitem("argent:piece_charbon", {
-    description = " piece en charbon",
-    inventory_image = "piece_charbon.png",
-})
-
-minetest.register_craft({
-    output = "argent:piece_charbon";
-    recipe = {
-        {"argent:poincon"},
-        {"default:coal_lump"},
-    };
-    replacements ={{"argent:poincon","argent:poincon"},
-    };
-});
-
---Tube de pièce en charbon (9 centimes) :
-minetest.register_craftitem("argent:tube_piece_charbon", {
-    description = "tube de 9 pieces en charbon",
-    inventory_image = "tube_piece_charbon.png",
-})
-
-minetest.register_craft({
-    output = "argent:tube_piece_charbon",
+    output = "argent:piece1 5",
     recipe =
         {
-        {"argent:piece_charbon","argent:piece_charbon","argent:piece_charbon"},
-        {"argent:piece_charbon","argent:piece_charbon","argent:piece_charbon"},
-        {"argent:piece_charbon","argent:piece_charbon","argent:piece_charbon"},
+        {"argent:piece5"},
     }
 })
 
-minetest.register_craft({
-    output = "argent:piece_charbon 9",
-    recipe = {
-        {"argent:tube_piece_charbon"},
-    }
-})
+--++++++++++++
+--+piece de 5|
+--++++++++++++
 
---Pièce en etain (10 centimes) :
-minetest.register_craftitem("argent:piece_etain", {
-    description = " piece en etain",
-    inventory_image = "piece_etain.png",
-})
-
-minetest.register_craft({
-    output = "argent:piece_etain";
-    recipe = {
-        {"argent:poincon"},
-        {"moreores:tin_ingot"},
-    };
-    replacements = {{"argent:poincon","argent:poincon"},
-    };
-});
-
-minetest.register_craft({
-    output = "argent:piece_etain",
-    recipe = {
-        {"argent:tube_piece_charbon","argent:piece_charbon"},
-    }
-})
-
---Tube de pièce en etain (90 centimes) :
-minetest.register_craftitem("argent:tube_piece_etain", {
-    description = "tube de 9 pieces en etain",
-    inventory_image = "tube_piece_etain.png",
-})
-
-minetest.register_craft({
-    output = "argent:tube_piece_etain",
-    recipe = {
-        {"argent:piece_etain","argent:piece_etain","argent:piece_etain"},
-        {"argent:piece_etain","argent:piece_etain","argent:piece_etain"},
-        {"argent:piece_etain","argent:piece_etain","argent:piece_etain"},
-    }
-})
-
-minetest.register_craft({
-    output = "argent:piece_etain 9",
-    recipe = {
-        {"argent:tube_piece_etain"},
-    }
-})
-
---Pièce en cuivre (50 centimes) :
-minetest.register_craftitem("argent:piece_cuivre", {
-    description = " piece en cuivre",
+--Declaration
+minetest.register_craftitem("argent:piece5", {
+    description = "piece de 5",
     inventory_image = "piece_cuivre.png",
 })
 
-minetest.register_craft({
-    output = "argent:piece_cuivre";
-    recipe = {
-        {"argent:poincon"},
-        {"moreores:copper_ingot"},
-    };
-    replacements = {{"argent:poincon","argent:poincon"},
-    };
-});
+--Conversion 
 
 minetest.register_craft({
-    output = "argent:piece_cuivre",
-    recipe = {
-        {"argent:piece_etain","argent:piece_etain","argent:piece_etain"},
-        {"argent:piece_etain","argent:piece_etain",""},
+	output = "argent:piece5",
+	recipe = 
+		{
+			{"","",""},
+			{"argent:piece1","argent:piece1",""},
+			{"argent:piece1","argent:piece1","argent:piece1"},
+	}
+})
+	
+--Conversion inverse
+minetest.register_craft({
+    output = "argent:piece5 2",
+    recipe =
+        {
+        {"argent:billet10"},
     }
 })
 
---Tube de pièces en cuivre (4€ 50) :
-minetest.register_craftitem("argent:tube_piece_cuivre", {
-    description = "tube de 9 pieces en cuivre",
-    inventory_image = "tube_piece_cuivre.png",
-})
+--++++++++++++++
+--|billet de 10|
+--++++++++++++++
 
-minetest.register_craft({
-    output = "argent:tube_piece_cuivre",
-    recipe = {
-        {"argent:piece_cuivre","argent:piece_cuivre","argent:piece_cuivre"},
-        {"argent:piece_cuivre","argent:piece_cuivre","argent:piece_cuivre"},
-        {"argent:piece_cuivre","argent:piece_cuivre","argent:piece_cuivre"},
-    }
-})
-
-minetest.register_craft({
-    output = "argent:piece_cuivre 9",
-    recipe = {
-        {"argent:tube_piece_cuivre"},
-    }
-})
-
---Pièce en acier (1€) :
-minetest.register_craftitem("argent:piece_acier", {
-description = " piece en acier",
-inventory_image = "piece_acier.png",
-})
-minetest.register_craft({
-    output = "argent:piece_acier";
-    recipe = {
-        {"argent:poincon"},
-        {"default:steel_ingot"},
-    };
-    replacements = {{"argent:poincon","argent:poincon"},
-    };
-});
-
-minetest.register_craft({
-    output = "argent:piece_acier",
-    recipe = {
-        {"argent:piece_cuivre","argent:piece_cuivre"},
-    }
-})
-
-minetest.register_craft({
-    output = "argent:piece_acier",
-    recipe = {
-        {"argent:tube_piece_etain","argent:piece_etain"},
-    }
-})
-
---Tube de pièces en acier (9€) :
-minetest.register_craftitem("argent:tube_piece_acier", {
-    description = "tube de 9 pieces en acier",
-    inventory_image = "tube_piece_acier.png",
-})
-
-minetest.register_craft({
-    output = "argent:tube_piece_acier",
-    recipe = {
-        {"argent:piece_acier","argent:piece_acier","argent:piece_acier"},
-        {"argent:piece_acier","argent:piece_acier","argent:piece_acier"},
-        {"argent:piece_acier","argent:piece_acier","argent:piece_acier"},
-    }
-})
-
-minetest.register_craft({
-    output = "argent:piece_acier 9",
-    recipe = {
-        {"argent:tube_piece_acier"},
-    }
-})
---**************************************************************************************
---Billet de 10
+--Declaration
 minetest.register_craftitem("argent:billet10", {
     description = " billet de 10",
     inventory_image = "billet10.png",
 })
 
+--Conversion :
 minetest.register_craft({
-    output="argent:billet10";
-    recipe= {
-        {"argent:tampon"},
-        {"moreores:silver_ingot"},
-        {"default:paper"},
-    };
-    replacements = {{"argent:tampon","argent:tampon"},
-    };
-});
-
-minetest.register_craft({
-    output="argent:billet10",
-    recipe= {
-        {"argent:tube_piece_acier","argent:piece_acier"},
+    output = "argent:billet10",
+    recipe =
+        {
+        {"argent:piece5","argent:piece5"},
     }
 })
 
+--Conversion inverse
 minetest.register_craft({
-    output="argent:billet10",
-    recipe= {
-        {"argent:tube_piece_cuivre","argent:piece_cuivre",""},
-        {"argent:tube_piece_cuivre","argent:piece_cuivre",""},
+    output = "argent:billet10 2",
+    recipe =
+        {
+        {"argent:billet20"},
     }
 })
 
---Billet de 20
+--++++++++++++++
+--|billet de 20|
+--++++++++++++++
+
+--Declaration
 minetest.register_craftitem("argent:billet20", {
     description = " billet de 20",
     inventory_image = "billet20.png",
 })
 
+--Conversion
 minetest.register_craft({
-    output="argent:billet20";
-    recipe= {
-        {"argent:tampon"},
-        {"moreores:gold_ingot"},
-        {"default:paper"},
-    };
-    replacements= {{"argent:tampon","argent:tampon"},
-    };
-});
-
-minetest.register_craft({
-    output="argent:billet20",
-    recipe= {
-        {"argent:billet10","argent:billet10"}
+    output = "argent:billet20",
+    recipe =
+        {
+        {"argent:billet10","argent:billet10"},
     }
 })
 
+--Conversion inverse
+minetest.register_craft({
+    output = "argent:billet20 2";
+    recipe = {
+        {"argent:billet50"},
+    };
+    replacements ={{"argent:billet50","argent:billet10"},
+    };
+});
 
---Billet de 50
+--++++++++++++++
+--|billet de 50|
+--++++++++++++++
+
+--Déclaration
 minetest.register_craftitem("argent:billet50", {
     description = " billet de 50",
     inventory_image = "billet50.png",
 })
 
+--Conversion
 minetest.register_craft({
-    output="argent:billet50";
-    recipe= {
-        {"argent:tampon"},
-        {"default:mese_crystal"},
-        {"default:paper"},
-    };
-    replacements= {{"argent:tampon","argent:tampon"},
-    };
-});
-
-minetest.register_craft({
-    output="argent:billet50",
-    recipe= {
-        {"argent:billet10","",""},
-        {"argent:billet10","argent:billet10",""},
-        {"argent:billet10","argent:billet10",""},
+    output = "argent:billet50",
+    recipe =
+       {
+		{"","",""},
+		{"argent:billet10","",""},
+		{"argent:billet20","argent:billet20",""},
     }
 })
 
+--Conversion inverse
 minetest.register_craft({
-    output="argent:billet50",
-    recipe= {
-        {"argent:billet20"},
-        {"argent:billet20"},
-        {"argent:billet10"},
+    output = "argent:billet50 2",
+    recipe =
+        {
+        {"argent:billet100"},
     }
 })
 
-minetest.register_craft({
-    output="argent:billet50",
-    recipe= {
-        {"argent:billet20","",""},
-        {"argent:billet10","",""},
-        {"argent:billet10","argent:billet10",""},
-    }
-})
+--+++++++++++++++
+--|billet de 100|
+--+++++++++++++++
 
---Billet de 100
+--Declaration
 minetest.register_craftitem("argent:billet100", {
     description = " billet de 100",
     inventory_image = "billet100.png",
 })
 
+--Conversion:
 minetest.register_craft({
-    output="argent:billet100";
-    recipe= {
-        {"argent:tampon"},
-        {"default:diamond"},
-        {"default:paper"},
-    };
-    replacements = {{"argent:tampon","argent:tampon"},
-    };
-});
-
-minetest.register_craft({
-    output="argent:billet100",
-    recipe= {
+    output = "argent:billet100",
+    recipe =
+        {
         {"argent:billet50"},
-        {"argent:billet50"},
+		{"argent:billet50"},
     }
 })
 
+--Conversion inverse
 minetest.register_craft({
-    output="argent:billet100",
-    recipe= {
-        {"argent:billet20","argent:billet20","argent:billet20"},
-        {"argent:billet20","argent:billet20",""},
+    output = "argent:billet100 2 ",
+    recipe =
+        {
+        {"argent:billet200"},
     }
 })
 
+--+++++++++++++++
+--|billet de 200|
+--+++++++++++++++
 
-
---Billet de 200
+--Declaration
 minetest.register_craftitem("argent:billet200", {
     description = " billet de 200",
     inventory_image = "billet200.png",
 })
 
+--Conversion
 minetest.register_craft({
-    output="argent:billet200";
-    recipe= {
-        {"","argent:tampon",""},
-        {"default:diamond","default:diamond",""},
-        {"","default:paper",""},
+    output = "argent:billet200",
+    recipe =
+        {
+        {"argent:billet100"},
+		{"argent:billet100"},
+    }
+})
+
+--Conversion inverse
+minetest.register_craft({
+    output = "argent:billet200 2";
+    recipe = {
+        {"argent:billet500"},
     };
-    replacements = {{"argent:tampon","argent:tampon"},
+    replacements ={{"argent:billet500","argent:billet100"},
     };
 });
 
-minetest.register_craft({
-    output="argent:billet200",
-    recipe= {
-        {"argent:billet50","argent:billet50","argent:billet50"},
-        {"argent:billet50","",""},
-    }
-})
+--+++++++++++++++
+--|billet de 500|
+--+++++++++++++++
 
-minetest.register_craft({
-    output="argent:billet200",
-    recipe= {
-        {"argent:billet100"},
-        {"argent:billet100"},
-    }
-})
-
-
---Billet de 500
+--Declaration
 minetest.register_craftitem("argent:billet500", {
     description = " billet de 500",
     inventory_image = "billet500.png",
 })
 
+--Conversion
 minetest.register_craft({
-    output="argent:billet500";
-    recipe= {
-        {"argent:tampon"},
-        {"default:mese_block"},
-        {"default:paper"},
-    };
-    replacements = {{"argent:tampon","argent:tampon"},
-    };
-});
-
-minetest.register_craft({
-    output="argent:billet500",
-    recipe= {
-        {"argent:billet100","argent:billet100","argent:billet100"},
-        {"argent:billet100","argent:billet100",""},
+    output = "argent:billet500",
+    recipe =
+        {
+		{"argent:billet100"},
+        {"argent:billet200"},
+		{"argent:billet200"},
     }
 })
-
-minetest.register_craft({
-    output="argent:billet500",
-    recipe= {
-        {"argent:billet200"},
-        {"argent:billet200"},
-        {"argent:billet100"},
-    }
-})
-
-
-
 --[[
+**********************************************************************************************
 
-Convertions :
-
-- 10 pièces en charbon = 1 pièce en etain
-- 50 pièces en charbon = 5 pièces en etain = 1 pièce en cuivre
-- 100 pièces en charbon = 10 pièces en etain = 2 pièces en cuivre = 1 pièce en acier
-- 1.000 pièces en charbon = 100 pièces en etain = 20 pièces en cuivre = 10 pièces en acier = 1 billet de 10 
-- 2.000 pièces en charbon = 200 pièces en etain = 40 pièces en cuivre = 20 pièces en acier = 1 billet de 20 
-- 5.000 pièces en charbon = 500 pièces en etain = 100 pièces en cuivre = 50 pièces en acier = 1 billet de 50 
-- 10.000 pièces en charbon = 1.000 pièces en etain = 200 pièces en cuivre = 100 pièces en acier = 1 billet de 100 
-- 20.000 pièces en charbon = 2.000 pièces en etain = 400 pièces en cuivre = 200 pièces en acier = 1 billet de 200 
-- 50.000 pièce en charbon = 5.000 pièces en etain = 1.000 pièces en cuivre = 500 pièces en acier = 1 billet de 500
-
+Machine à cash !
+Cube qui permet d'obtenir de l'argent ( pièce de 1 ) en échange de cobble
 ]]--
+
+minetest.register_node("argent:cash", {
+	description = "Echangeur de cobble",
+	tiles = {"cash.png"},
+	is_ground_content = false,
+	walkable = true,
+	pointable = true,
+	diggable = false,
+	groups = {cracky=2,crumbly=3},
+	drop = "argent:cash" ,
+})
+minetest.register_on_punchnode(function(p, node, player)
+	if node.name=="argent:cash" and player:get_inventory():contains_item('main', 'default:cobble') then
+		player:get_inventory():add_item('main', 'argent:piece1 3' )
+		player:get_inventory():remove_item('main', 'default:cobble')
+	end
+
+end)
